6eff7bdf45e47be2178dd29ee11e3136a20461f2
kathure <juliekmuthee@gmail.com>
2019-07-15 11:46:19 +0300

    created pipe fvrt-txt

---
+++ src/app/fvrt-txt.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FvrtTxtPipe } from './fvrt-txt.pipe';
+
+describe('FvrtTxtPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FvrtTxtPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
